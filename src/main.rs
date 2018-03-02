4a8584c110faf963c9066aeacc22045cf7009a95
javiglesias <javiglesias@outlook.com>
2018-03-02 20:57:54 +0100

    Doing insertion...
    To be continued...

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use std::io;
 mod lib;
+/*fn swap(a:&mut i32, b:&mut i32) {
+    let temp = a;
+    a = b;
+    b = temp; 
+}*/
+fn insertion(disorder:&mut Vec<i32>) -> bool{
+    let mut i = 0;
+    while i < disorder.len() {
+        let x = disorder[i];
+        let j = disorder.len();
+        let mut choosen;
+        while j < i {
+            if x < disorder[j]{
+                choosen = &disorder[j];
+            }
+        }
+        i+=1;
+    }
+    true
+}
 fn blub(disorder:&mut Vec<i32>) -> bool{
     let mut done:bool = false;
     let mut n = disorder.len();
@@ -32,9 +52,15 @@ fn blub(disorder:&mut Vec<i32>) -> bool{
 }
 fn main() {
     println!("ORDENACION");
+    let default = vec![10,4,2,5,6,7,3,9,1,8];
     let mut disorder = vec![10,4,2,5,6,7,3,9,1,8];
     println!("Pre-Burbuja: {:?}",disorder);
     if blub(&mut disorder) {
         println!("Post-Burbuja: {:?}",disorder);
     } 
+    disorder = default;
+    println!("Pre-Insertion: {:?}", disorder);
+    if insertion(&mut disorder) {
+        println!("Post-Insertion: {:?}", disorder);
+    }
 }
